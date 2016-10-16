c6d34362272777760f90633d5e1d7c8a650a2df4
SchweigerJul <schweigerjul@gmail.com>
2016-10-16 19:16:40 +0200

    First Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :expenses
+  get 'expenses/index'
+
+  root 'expenses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20161016171234_create_expenses.rb
@@ -0,0 +1,12 @@
+class CreateExpenses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :expenses do |t|
+      t.string :store
+      t.text :description
+      t.date :date
+      t.float :value
+
+      t.timestamps
+    end
+  end
+end
